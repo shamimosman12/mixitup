0822bd4fc3a9fed8e8b95f44d22774aa29073261
Shamim Hossain <167690026+shamimosman12@users.noreply.github.com>
2024-10-23 17:17:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*--------js--------*/
+
+
+
+
+
+/*---------jq--------*/
+$(document).ready(function(){
+
+	var mixer = mixitup('#hello', {
+
+    animation: {
+        duration: 300
+    }
+});
+
+     lightbox.option({
+      'resizeDuration': 300,
+      'wrapAround': true
+    })
+});
